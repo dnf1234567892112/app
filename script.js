097f905b043527c44d9ab0da2cedc94eae30966c
durandesh <dnf1234@bk.ru>
2022-06-05 04:27:09 +0300

    js commit

--- script.js
+++ script.js
@@ -8,11 +8,31 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+for(let i = 0; i < 2; i++){
 
-let resul = prompt('Один из последних просмотренных фильмов');
-let filmScore = prompt('На сколько оцените его');
+   
+    let resul = prompt('Один из последних просмотренных фильмов');
+    let filmScore = prompt('На сколько оцените его');
+    if(resul !=null && filmScore!=null && resul!=''&& resul.length <50){
+    personalMovieDB.movies[resul] = filmScore;
+        console.log('Done');
+}
+    else{
+        i--;
+        console.log('error');
+    }
+}
 
-personalMovieDB.movies[resul] = filmScore;
-
-
+    if(personalMovieDB.count <= 10){
+        console.log('Просмотрено довольно мало фильмов');
+    }
+        else if(personalMovieDB.count >=10 && personalMovieDB.count < 30){
+            console.log('Вы классический зритель')
+        }
+        else if(personalMovieDB.count > 50){
+            console.log('Вы киноман');
+        }
+        else{
+            console.log('Произашло ошибка');
+        }
 console.log(personalMovieDB);
